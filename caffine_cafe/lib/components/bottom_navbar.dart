ec4cfe57a4d17879681477263dbe03f235330ce1
Lakshita <lakshita2119@gmail.com>
2023-09-03 03:44:36 +0530

    Update bottom_navbar.dart

--- caffine_cafe/lib/components/bottom_navbar.dart
+++ caffine_cafe/lib/components/bottom_navbar.dart
@@ -6,14 +6,25 @@ class MyBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GNav(tabs: [
-      GButton(icon: Icons.home,
-      text: 'Shop',),
-
-      GButton(icon: Icons.shopping_bag_outlined,
-      text: 'Cart',),
-
-    ]
-    ) ;
+    return Container(
+      margin: const EdgeInsets.all(25),
+      child: GNav(
+          color: const Color.fromRGBO(189, 189, 189, 1),
+          mainAxisAlignment: MainAxisAlignment.center,
+          activeColor: const Color.fromRGBO(97, 97, 97, 1),
+          tabBackgroundColor: const Color.fromRGBO(224, 224, 224, 1),
+          tabBorderRadius: 24,
+          tabActiveBorder: Border.all(color: Colors.white),
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: 'Shop',
+            ),
+            GButton(
+              icon: Icons.shopping_bag_outlined,
+              text: 'Cart',
+            ),
+          ]),
+    );
   }
 }
