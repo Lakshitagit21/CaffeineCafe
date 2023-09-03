e7d5bf7fcd2db3c2f5f373315dec6623ea3fa64c
Lakshita <lakshita2119@gmail.com>
2023-09-03 18:29:58 +0530

    made intro page

--- caffine_cafe/lib/main.dart
+++ caffine_cafe/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:caffine_cafe/pages/home_page.dart';
+import 'package:caffine_cafe/pages/intro_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: IntroPage(),
     );
   }
 }
---
+++ caffine_cafe/lib/pages/intro_page.dart
@@ -0,0 +1,77 @@
+import 'package:caffine_cafe/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Padding(
+              padding: const EdgeInsets.fromLTRB(65, 25, 25, 25),
+              child: Image.asset(
+                'lib/images/hot-coffee.png',
+                height: 160,
+              ),
+            ),
+
+            const SizedBox(
+              height: 40,
+            ),
+
+            //text
+            const Text(
+              'Brew Day',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 73, 49, 40),
+                fontSize: 25,
+              ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.all(25.0),
+              child: Text(
+                'How do you like your coffee?',
+                style: TextStyle(color: Colors.brown),
+              ),
+            ),
+
+            //button
+
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: GestureDetector(
+                onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    )),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 39, 26, 21),
+                      borderRadius: BorderRadius.circular(12)),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
