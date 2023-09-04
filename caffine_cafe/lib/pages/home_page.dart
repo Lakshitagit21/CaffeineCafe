39154a96946111b11bd83ccc984c4221ec019495
Lakshita <lakshita2119@gmail.com>
2023-09-05 01:46:02 +0530

    bottom navbar completed

--- caffine_cafe/lib/pages/home_page.dart
+++ caffine_cafe/lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:caffine_cafe/components/bottom_navbar.dart';
+import 'package:caffine_cafe/pages/shop_page.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,12 +12,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottombar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 224, 224, 224), 
-      
-    bottomNavigationBar: MyBottomNavBar(),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 224, 224, 224),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottombar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
