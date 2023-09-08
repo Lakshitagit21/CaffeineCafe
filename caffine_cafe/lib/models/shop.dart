0224ee8042fc94788e0ea0c17f0e2c2cc7d16b44
Lakshita <lakshita2119@gmail.com>
2023-09-08 16:55:26 +0530

    provider

---
+++ caffine_cafe/lib/models/shop.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'drink.dart';
+
+class CoffeeShop extends ChangeNotifier {
+  //ist of drinks for sale
+  final List<Drink> _shop = [
+    //iced-coffee
+    Drink(
+      name: "Iced- coffee",
+      price: "4.00",
+      imagePath: "lib/images/iced-coffee.png",
+    ),
+  ];
+
+  //list of drinks in user cart
+  final List<Drink> _userCart = [];
+
+  //get drinks for sale
+  List<Drink> get shop => _shop;
+
+  //get user cart
+  List<Drink> get cart => _userCart;
+
+  //add drink to cart
+  void addToCart(Drink drink) {
+    _userCart.add(drink);
+    notifyListeners();
+  }
+
+  //remove  drink from cart
+  void removeFromCart(Drink drink) {
+    _userCart.remove(drink);
+    notifyListeners();
+  }
+}
--- caffine_cafe/lib/pages/shop_page.dart
+++ caffine_cafe/lib/pages/shop_page.dart
@@ -10,6 +10,24 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center( child: Text('Shop'),);
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+      
+          children: [
+      
+            //heading
+            Text('How do you like your Coffee?', style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold,
+            ),
+            ),
+      
+            //list of coffee
+            
+          ],
+        ),
+      ),
+    );
   }
 }
